85f7911a1048f2a0e125c506b9a87cde9db52b0d
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-07-22 12:40:18 +0300

    Add loader.py to load files and extract the text

---
+++ src/ingestion/loader.py
@@ -0,0 +1,37 @@
+import fitz
+import os
+
+print("test")
+
+def load_documents(folder_path):
+
+    documents = []
+
+    pdf_files = os.listdir(folder_path) # Get all files
+
+    # print(pdf_files)
+
+    for file in pdf_files: # For each file in files
+
+        if file.endswith(".pdf") or file.endswith(".txt"):
+
+            pdf_path = os.path.join(folder_path, file) # Get the file full path
+
+            doc = fitz.open(pdf_path) # Open the pdf file
+
+            text = ""
+
+            # Get all pages of the file, one by one
+            for page in doc: 
+
+                text += page.get_text()
+
+            documents.append({
+                "filename": file,
+                "text": text
+            })
+
+            doc.close()
+
+    return documents
+
