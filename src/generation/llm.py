e1f841ff3809e5cdfbc536cf081f4da3121e08a2
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-07-23 14:00:21 +0300

    Complete the RAG system

---
+++ src/generation/llm.py
@@ -0,0 +1,45 @@
+
+
+
+import requests
+
+
+
+def generate_answer(question, context):
+    # question --> the user question 
+    # context → the text retrieved from ChromaDB
+
+
+    # Create the prompt 
+    prompt = f"""
+    You are an AI assistant.
+    
+    Answer the question using only the provided context.
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer:
+    """
+
+    # Sending the request to Ollama
+    response = requests.post(
+        # This is Ollama's API running on your own computer.
+        # So we are talking directly to the local Mistral model.
+        "http://localhost:11434/api/generate",
+        # Sending information
+        json = {
+            "model": "mistral",
+            "prompt": prompt,
+            "stream": False # means ollama wait until the full answer is finished, then send it back.
+        }
+
+    )
+
+    # Ollama returns JSON file that has the model name, reponse (the answer of the prompt), done status.
+    # Now we want to extract the answer from the response JSON file from Ollama.
+    return response.json()["response"]
+
