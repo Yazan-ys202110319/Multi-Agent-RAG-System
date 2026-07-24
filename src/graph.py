9e336eb8e2381f5eba50f60ab90d5392ec7d878d
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-07-25 00:49:26 +0300

    Create graph.py and add the state skeleton and create the nodes

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,93 @@
 # This file will decides which agent runs next and the graph.
 
 
+from typing import TypedDict
+
+from langgraph.graph import State, START, END
+
+
+from src.agents.retrieval_agent import retrieval_agent
+from src.agents.reasoning_agent import reasoning_agent
+from src.agents.validation_agent import validation_agent
+
+
+# Step 1- Creating the state
+
+class AgentState(TypedDict):
+
+    question: str
+    chunks: str
+    answer: str
+    valid: str
+
+
+# Step 2- Creating the nodes for each agent
+
+# Retrieval node
+def retrieval_agent(state: AgentState):
+
+    # Get the question from the state
+    chunks = retrieval_agent(state["question"])
+
+    # Now call the retrieval agent to get the related chunks to the question and store it in the (chunk) states
+    
+    state["chunks"] = chunks
+
+    return state
+
+# question
+#    |
+#    ↓
+# retrieval_agent()
+#    |
+#    ↓
+# chunks
+
+
+
+# Reasoning node
+
+def reasoning_node(state: AgentState):
+
+    answer = reasoning_agent(state["question"], state["chunks"])
+
+    state["answer"] = answer
+
+    return state
+
+# question + chunks
+#         |
+#         ↓
+#  reasoning_agent()
+#         |
+#         ↓
+#      answer
+
+# Validation node
+def validation_node(state: AgentState):
+
+    results = validation_agent(state["answer"], state["chunks"])
+
+    # Get the final answer and validation from the agent, the answer might change in case the valid = False (check the agent code)
+
+    # If valid = True, the answer will be as it is coming from reasoning_node (so without chaning) 
+    state["answer"] = results["answer"]
+    state["valid"] = results["valid"]
+
+
+    return state
+
+
+# answer + chunks
+#        |
+#        ↓
+# validation_agent()
+#        |
+#        ↓
+# valid / invalid
+
+# Step 3- Build the graph
+
+
 
 
